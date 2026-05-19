bf3ec30e374c084d010794e3fbbf3c03722d4d37
Aurythx <wearauryth@gmail.com>
2026-05-19 00:47:05 -0500

    Update ThunderstormBackground.tsx

--- ThunderstormBackground.tsx
+++ ThunderstormBackground.tsx
@@ -3,7 +3,7 @@ import ThunderstormBackground from "@/components/ThunderstormBackground";
 export default function Home() {
   return (
     <main style={{ position: "relative", minHeight: "100vh" }}>
-      <ThunderstorBackground />
+      <ThunderstormBackground />
 
       {/* Your existing homepage content goes here, on top of the storm */}
       <div style={{ position: "relative", zIndex: 1 }}>
