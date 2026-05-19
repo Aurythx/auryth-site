f10dc45fdfb6dfd81d93e2fe6754732539a80158
Aurythx <wearauryth@gmail.com>
2026-05-19 11:20:11 -0500

    Trash this

--- ThunderstormBackground.tsx
+++ ThunderstormBackground.tsx
@@ -1,14 +1,1 @@
-import ThunderstormBackground from "@/components/ThunderstormBackground";
 
-export default function Home() {
-  return (
-    <main style={{ position: "relative", minHeight: "100vh" }}>
-      <ThunderstormBackground />
-
-      {/* Your existing homepage content goes here, on top of the storm */}
-      <div style={{ position: "relative", zIndex: 1 }}>
-        {/* your content */}
-      </div>
-    </main>
-  );
-}
